4df7e5da2f5f1f04c5d5211d8485156d43bfa3b3
pahagon <paulo.ahagon@gmail.com>
2025-04-04 10:27:20 -0300

    chor(web): Removes default value of redis host settings

--- web/main.py
+++ web/main.py
@@ -8,9 +8,15 @@ from wsgiref.simple_server import make_server
 host = os.getenv('FR_APP_HOST') or "0.0.0.0"
 port = os.getenv('FR_APP_PORT') or 5000
 
-redis_host = os.getenv('FR_REDIS_HOST') or "127.0.0.1"
-redis_port = os.getenv('FR_REDIS_PORT') or 6379
-redis_cluster = os.getenv('FR_REDIS_CLUSTER') or "false"
+redis_host = os.getenv('FC_REDIS_HOST')
+redis_port = os.getenv('FC_REDIS_PORT')
+redis_cluster = os.getenv('FC_REDIS_CLUSTER')
+
+redis_client = None
+if redis_cluster.lower() == "true":
+    redis_client = redis.cluster.RedisCluster(startup_nodes=[{"host": redis_host, "port": redis_port}], decode_responses=True)
+else:
+    redis_client = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
 
 allow_origins = [
     "http://{host}:{port}".format(host=host, port=port),
@@ -78,21 +84,13 @@ def handle_embeddings(environ, start_response):
 
     return [b"Method Not Allowed"]
 
-
-redis_client = None
-if redis_cluster.lower() == "true":
-    redis_client = redis.cluster.RedisCluster(startup_nodes=[{"host": redis_host, "port": redis_port}], decode_responses=True)
-else:
-    redis_client = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
-
 def query_embeddings(face_encoding):
-    base_query = "*=>[KNN 2 @embedding $vec_param AS vector_score]"
     face_encoding_float32 = np.array(face_encoding, dtype=np.float32)
     query_vector = face_encoding_float32.tobytes()
     query_params = {"vec_param": query_vector}
     ret = redis_client.execute_command(
             "FT.SEARCH", "idx:embeddings",
-            base_query,
+            "*=>[KNN 2 @embedding $vec_param AS vector_score]",
             "PARAMS", "2", "vec_param", query_vector,
             "RETURN", "1", "vector_score",
             "SORTBY", "vector_score",
